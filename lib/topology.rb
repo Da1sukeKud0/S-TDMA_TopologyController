781c5c60311069fcdb1459462aeca7badbb5d096
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-10-09 18:58:38 +0900

    [fix] @topoでs2s,s2hのリンク情報を保持

--- lib/topology.rb
+++ lib/topology.rb
@@ -27,9 +27,12 @@ class Topology
   def initialize
     @observers = []
     @ports = Hash.new { [].freeze }
+    ## links[]: all links (only s2s)
     @links = []
-    # @hosts = []
+    ## hosts{}: keyはホストのmac_address
     @hosts = Hash.new { [].freeze }
+    ## topo[]: all links (s2s and s2h)
+    @topo = []
   end
 
   def add_observer(observer)
@@ -65,11 +68,13 @@ class Topology
   def maybe_add_link(link)
     return if @links.include?(link)
     @links << link
+    ## topo
+    add_switch2switch_link link
     port_a = Port.new(link.dpid_a, link.port_a)
     port_b = Port.new(link.dpid_b, link.port_b)
     maybe_send_handler :add_link, port_a, port_b, self
   end
-  
+
   def maybe_add_host(*host)
     # return if @hosts.include?(host)
     return if @hosts.key?(host[0])
@@ -80,44 +85,110 @@ class Topology
     hostStats.store(:dpid, host[2])
     hostStats.store(:port_no, host[3])
     @hosts[host[0]] = hostStats
+    ## topo
+    add_switch2host_link hostStats
     mac_address, _ip_address, dpid, port_no = *host
     maybe_send_handler :add_host, mac_address, Port.new(dpid, port_no), self
   end
-  
+
   def route(ip_source_address, ip_destination_address)
     @graph.route(ip_source_address, ip_destination_address)
   end
-  
+
   ## topologyをJSON形式で出力
-  def show_links
-    ret = []
-    @links.each do |each|
-      node = Hash.new { [].freeze }
-      node.store(:type, "switch2switch")
-      node.store(:id_a, each.dpid_a)
-      node.store(:port_a, each.port_a)
-      node.store(:id_b, each.dpid_b)
-      node.store(:port_b, each.port_b)
-      ret.push(node)
-    end
-    @hosts.each do |key,value|
-      node = Hash.new { [].freeze }
-      node.store(:type, "switch2host")
-      ## Switch
-      node.store(:id_a, value[:dpid])
-      node.store(:port_a, value[:port_no])
-      ## Host
-      node.store(:id_b, value[:mac_address])
-      ret.push(node)
-    end
-    File.open("/tmp/topology.json", 'w') do |file|
-      JSON.dump(ret, file)
+  # def show_links
+  #   start_time = Time.now ## benchmarc
+  #   ret = []
+  #   @links.each do |each|
+  #     l = Hash.new { [].freeze }
+  #     l.store(:type, "switch2switch")
+  #     l.store(:id_a, each.dpid_a)
+  #     l.store(:port_a, each.port_a)
+  #     l.store(:id_b, each.dpid_b)
+  #     l.store(:port_b, each.port_b)
+  #     ret.push(l)
+  #   end
+  #   @hosts.each do |key, value|
+  #     l = Hash.new { [].freeze }
+  #     l.store(:type, "switch2host")
+  #     ## Switch
+  #     l.store(:id_a, value[:dpid])
+  #     l.store(:port_a, value[:port_no])
+  #     ## Host
+  #     l.store(:id_b, value[:mac_address])
+  #     ret.push(l)
+  #   end
+  #   File.open("/tmp/topology.json", "w") do |file|
+  #     JSON.dump(ret, file)
+  #   end
+  #   puts "time: #{Time.now - start_time}"
+  # end
+
+  ## topo
+  ## @topoをJSON形式で出力する関数
+  ##
+  def output_Topo
+    puts @topo
+    File.open("/tmp/topology.json", "w") do |file|
+      JSON.dump(@topo, file)
     end
   end
-  
+
   private
-  
+
+  ## topo
+  ## s2sのリンクを追加する関数
+  ##
+  def add_switch2switch_link(link)
+    l = Hash.new { [].freeze }
+    l.store(:type, "switch2switch")
+    l.store(:id_a, link.dpid_a)
+    l.store(:port_a, link.port_a)
+    l.store(:id_b, link.dpid_b)
+    l.store(:port_b, link.port_b)
+    @topo.push(l)
+    output_Topo
+  end
+
+  ## topo
+  ## s2hのリンクを追加する関数
+  ##
+  def add_switch2host_link(hostStats)
+    l = Hash.new { [].freeze }
+    l.store(:type, "switch2host")
+    ## Switch
+    l.store(:id_a, hostStats[:dpid])
+    l.store(:port_a, hostStats[:port_no])
+    ## Host (s2hの場合はid_portはなし)
+    l.store(:id_b, hostStats[:mac_address])
+    @topo.push(l)
+    output_Topo
+  end
+
+  ## topo
+  ## s2s,s2hのリンクを追加する関数
+  ##
+  def delete_switch2switch_link(port)
+    for each in @topo do
+      ## id_a, port_aおよびid_b, port_bと一致した場合に
+      ## @topoからリンクを削除
+      if ((each[:id_a] == port.dpid) && (each[:port_a] == port.number))
+        @topo -= [each]
+        ## s2hの場合は@hostsからホストを削除
+        if (each[:type] == "switch2host")
+          @hosts.delete(each[:id_b])
+        end
+        output_Topo
+      elsif (each[:id_b] == port.dpid) && (each[:port_b] == port.number)
+        @topo -= [each]
+        output_Topo
+      end
+    end
+  end
+
   def maybe_delete_link(port)
+    ## topo
+    delete_switch2switch_link port
     @links.each do |each|
       next unless each.connect_to?(port)
       @links -= [each]
@@ -126,14 +197,13 @@ class Topology
       maybe_send_handler :delete_link, port_a, port_b, self
     end
   end
-  
+
   def maybe_send_handler(method, *args)
     @observers.each do |each|
       if each.respond_to?(:update)
         each.__send__ :update, method, args[0..-2], args.last
       end
       each.__send__ method, *args if each.respond_to?(method)
-      show_links
     end
   end
 end
