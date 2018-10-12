852b191df02d2c64288ca6ef59e5f97a77e2baaa
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-10-13 02:40:01 +0900

    [fix] Minor fix

--- lib/dijkstra.rb
+++ lib/dijkstra.rb
@@ -1,5 +1,3 @@
-require "json"
-
 class Graph
   attr_reader :graph, :nodes, :previous, :distance # getter
   INFINITY = 1 << 64
@@ -9,19 +7,7 @@ class Graph
     @nodes = []
   end
 
-  ##
-  ## 各ノード
-  ##
-  def connect_graph(src, dst, weight)
-    if !graph.key?(src)
-      graph[src] = {dst => weight}
-    else
-      graph[src][dst] = weight
-    end
-    nodes.push(src) unless nodes.include?(src)
-  end
-
-  ##
+  ## main
   ## 無向グラフを作成
   ##
   def add_edge(src, dst, weight)
@@ -31,6 +17,9 @@ class Graph
     connect_graph(dst, src, weight) ## dst -> src
   end
 
+  ## main
+  ## ダイクストラ法
+  ##
   def dijkstra(src)
     @distance = {}
     @previous = {}
@@ -51,26 +40,20 @@ class Graph
         alt = @distance[u] + graph[u][vertex]
         if alt < @distance[vertex]
           @distance[vertex] = alt
-          @previous[vertex] = u # A shorter path to v has been found
+          @previous[vertex] = u # ノードvへの最短パス
         end
       end
     end
   end
 
-  def find_path(dst)
-    if (@previous[dst] != -1)
-      find_path @previous[dst]
-    end
-    @path << dst
-  end
-
-  ##
+  ## main
   ## 最短パスを探索
   ##
-  def shortest_paths(src, dst)
+  def shortest_path(src, dst)
     src = src.to_s
     dst = dst.to_s
-    @graph_paths = []
+    ## 経路の解を格納
+    @solved_paths = []
     @src = src
     dijkstra src
     # nodes.each do |dst|
@@ -81,14 +64,32 @@ class Graph
     else
       actual_distance = "no path"
     end
-    @graph_paths.push("dst" => dst, "path" => @path, "cost" => actual_distance)
+    @solved_paths.push("src" => src, "dst" => dst, "path" => @path, "cost" => actual_distance)
     # end
+    output_cli
     @graph_paths
   end
 
+  private
+  def connect_graph(src, dst, weight)
+    if !graph.key?(src)
+      graph[src] = {dst => weight}
+    else
+      graph[src][dst] = weight
+    end
+    nodes.push(src) unless nodes.include?(src)
+  end
+
+  def find_path(dst)
+    if (@previous[dst] != -1)
+      find_path @previous[dst]
+    end
+    @path << dst
+  end
+
   def output_cli
-    @graph_paths.each do |graph|
-      puts graph
+    @solved_paths.each do |each|
+      puts each
     end
   end
 
@@ -97,6 +98,18 @@ class Graph
   end
 end
 
+## private
+## jsonを展開し読み込み
+##
+private def getJSON
+File.open("test/topology.json") do |file|
+  return JSON.load(file)
+end
+end
+
+## test
+## native source code test
+##
 def test
   g = Graph.new
   g.add_edge("a", "c", 7)
@@ -109,50 +122,31 @@ def test
   g.add_edge("f", "e", 2)
   g.add_edge("e", "b", 9)
   g.shortest_paths("a", "b")
-  g.output_cli
 end
 
-def getJSON
-  File.open("test/topology.json") do |file|
-    return JSON.load(file)
-  end
-end
-
-##
+## test
 ## JSON test
 ##
-topo = getJSON
-puts "-- topology --"
-puts topo
-puts ""
+def testJSON
+  topo = getJSON
+  puts "-- topology --"
+  puts topo
+  puts ""
 
-puts "-- hostname => mac_address --"
-g = Graph.new
-mac_table = Hash.new
-for each in topo
-  if (each["type"] == "switch2host")
-    mac_table["h" + (mac_table.size + 1).to_s] = {"id_a" => each["id_a"], "mac_address" => each["id_b"]}
+  puts "-- hostname => mac_address --"
+  g = Graph.new
+  mac_table = Hash.new
+  cost = [2, 3, 4, 6, 6, 5, 2, 2, 4] ## for test
+  for each in topo
+    if (each["type"] == "switch2host") ## s2h
+      mac_table["h" + (mac_table.size + 1).to_s] = {"id_a" => each["id_a"], "mac_address" => each["id_b"]}
+    else ## s2s
+      g.add_edge(each["id_a"], each["id_b"], cost.shift)
+    end
   end
-end
-puts mac_table
-puts ""
+  puts mac_table
+  puts ""
 
-puts "-- dijk --"
-cost = [2, 3, 4, 6, 6, 5, 2, 2, 4]
-for each in topo
-  if (each["type"] == "switch2switch")
-    g.add_edge(each["id_a"], each["id_b"], cost.shift)
-  end
+  puts "-- dijk --"
+  g.shortest_path(1, 5)
 end
-# g.add_edge("1", "2", 5)
-# g.add_edge("1", "3", 4)
-# g.add_edge("1", "4", 2)
-# g.add_edge("2", "3", 2)
-# g.add_edge("2", "5", 6)
-# g.add_edge("3", "4", 3)
-# g.add_edge("3", "6", 2)
-# g.add_edge("4", "6", 6)
-# g.add_edge("5", "6", 4)
-g.shortest_paths(1, 5)
-g.output_cli
-g.output_path
