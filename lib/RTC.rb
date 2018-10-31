c1a556897bba38f7f71586cec0eb73678987ddf4
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-10-31 18:38:06 +0900

    [fix] 仕様変更(hst_idの追加)に関する変更

--- lib/RTC.rb
+++ lib/RTC.rb
@@ -2,12 +2,14 @@
 ## 送信元/宛先idおよび通信周期は初期入力
 ## スケジューリング可能であれば通信経路とタイムスロットIDを獲得
 class RTC
-  attr_reader :route
+  attr_reader :src_id
+  attr_reader :dst_id
+  attr_reader :period
   attr_reader :timeslot_id
 
   def initialize(src_id, dst_id, period)
-    @src_id = src_id ## dpidの予定
-    @dst_id = dst_id ## dpidの予定
+    @src_id = src_id ## hst_idの予定
+    @dst_id = dst_id ## hst_idの予定
     @period = period ## 通信周期（タイムスロット単位）
     @schedule = nil ## スケジューリング可能な場合にsetScheduleで格納
   end
