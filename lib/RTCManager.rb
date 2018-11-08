a819e42004fbb53a9dc5919c84a46d7b865856ab
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-11-08 14:05:30 +0900

    [hotfix] 経路スケジューリング機構の完成（Dijkstra.new(@map)+setGraphを個々のタイムスロット計算時に新規生成・@timeslot_tableを10ブロックに設定）

--- lib/RTCManager.rb
+++ lib/RTCManager.rb
@@ -6,9 +6,11 @@ require "~/trema/topology/lib/dijkstra"
 
 class RTCManager
   def initialize
-    @map = Dijkstra.new ## 経路計算アルゴリズム用に変換されたトポロジ情報
     @hst_table = Hash.new ## ホストの識別名(h1,h2,,,hn)とmac_addressの対応
-    @rtcList = [] ## 実時間通信タスク
+    @timeslot_table = Hash.new ##timeslot_id: [rtc,rtc,,,]
+    for i in Range.new(0, 9)
+      @timeslot_table[i] = []
+    end
   end
 
   attr_reader :map
@@ -18,49 +20,98 @@ class RTCManager
   ## 可能ならrtcListを更新しtrue 不可ならfalse
   def add_rtc?(src, dst, period, topo)
     rtc = RTC.new(src, dst, period)
-    routeSchedule(rtc, topo)
-    periodSchedule(rtc)
-    if rtc.schedulable? ## スケジューリング可能
-      @rtcList.push(rtc)
-      return true
-    else ## スケジューリング不可
+    if !routeSchedule(rtc, topo)
+      puts "####################"
+      puts "####################"
+      puts "####### false ######"
+      puts "####################"
+      puts "####################"
       return false
     end
+    puts @timeslot_table
+    return true
   end
 
   private
 
   ## 経路スケジューリング：既存の実時間通信との非重複経路を探索
-  ## 可能ならrtc.scheduleに経路とタイムスロットIDを追加する
+  ## 可能ならrtcにroute, initial_phaseを追加しtrue, 不可ならfalse
   def routeSchedule(rtc, topo)
-    setGraph(topo)
-    if (@rtcList.size == 0)
-      @map.shortest_path(@hst_table.invert(rtc.src), @hst_table.invert(rtc.dst))
-    else
-      @rtcList.each do |each|
+    ##既存のrtcがない場合
+    if (@timeslot_table.all? { |key, each| each.size == 0 })
+      map = setGraph(topo)
+      route = map.shortest_path(@hst_table.key(rtc.src), @hst_table.key(rtc.dst))
+      if (route) ##ルートあり
+        initial_phase = 0 ##初期位相0に設定
+        rtc.setSchedule(initial_phase, route)
+        for i in Range.new(initial_phase, 9)
+          if ((i + initial_phase) % rtc.period == 0)
+            @timeslot_table[i].push(rtc)
+          end
+        end
+      else ##ルートなし
+        return false
+      end
+    else ## 既存のrtcがある場合
+      route_list = Hash.new()
+      @timeslot_table.each do |timeslot, exist_rtcs|
+        ## initial_phase==0として、timeslotが被るrtcがあれば抽出し使用ルートを削除してから探索
+        puts "tsl=#{timeslot}"
+        if (timeslot % rtc.period == 0)
+          map_tmp = setGraph(topo)
+          if (exist_rtcs.size != 0) ## 同一タイムスロット内にrtcが既存
+            puts "既存のRTCあるよ"
+            for each_rtc in exist_rtcs
+              puts "each_rtc=#{each_rtc}"
+              for e in each_rtc.route
+                map_tmp.delete_edge(e[:src], e[:dst])
+              end
+            end
+          end
+          ## 既存の使用ルートを除いてから再計算
+          route = map_tmp.shortest_path(@hst_table.key(rtc.src), @hst_table.key(rtc.dst))
+          if (route) ## ルーティング可能なら一時変数に格納
+            route_list[timeslot] = route
+          else
+            return false
+          end
+        end
+      end
+      ## ここでfalseでない時点で0~9のうち使用する全てのタイムスロットでルーティングが可能
+      route_list.each do |key, val|
+        initial_phase = 0
+        rtc_tmp = RTC.new(rtc.src, rtc.dst, rtc.period)
+        rtc_tmp.setSchedule(initial_phase, val)
+        @timeslot_table[key].push(rtc_tmp)
       end
     end
+    return true
   end
 
   def setGraph(topo)
+    map = Dijkstra.new ## 経路計算アルゴリズム用に変換されたトポロジ情報
     for each in topo
       if (each[:type] == "switch2host")
         ## s2hリンクの場合
-        hst_id = "h" + (@mac_table.size + 1).to_s
-        @hst_table[hst_id] = each[:host]
-        @map.add_edge(each[:switch_a][:dpid], hst_id, 0) ## cost 0
+        if @hst_table.value?(each[:host])
+          hst_id = @hst_table.key(each[:host])
+        else
+          hst_id = "h" + (@hst_table.size + 1).to_s
+          @hst_table[hst_id] = each[:host]
+        end
+        map.add_edge(each[:switch_a][:dpid], hst_id, 0) ## cost 0
         puts "#{each[:switch_a][:dpid]} to #{hst_id} link add"
       else
         ## s2sリンクの場合
-        @map.add_edge(each[:switch_a][:dpid], each[:switch_b][:dpid]) ## cost 1
+        map.add_edge(each[:switch_a][:dpid], each[:switch_b][:dpid]) ## cost 1
         puts "#{each[:switch_a][:dpid]} to #{each[:switch_b][:dpid]} link add"
       end
     end
+    return map
   end
 
   ## 時刻スケジューリング：他の実時間通信の周期と被らないように新規タイムスロットを設定
   def periodSchedule(rtc)
-    
   end
 end
 
