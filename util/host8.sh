db0781ce08384cc51a66905da79cffc3187ea5b8
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-11-08 13:32:15 +0900

    [add] 非重複経路の探索テスト

---
+++ util/host8.sh
@@ -0,0 +1,9 @@
+rvmsudo ./bin/trema send_packets -n 1 -s h1 -d h2
+rvmsudo ./bin/trema send_packets -n 1 -s h2 -d h1
+rvmsudo ./bin/trema send_packets -n 1 -s h3 -d h1
+rvmsudo ./bin/trema send_packets -n 1 -s h4 -d h1
+rvmsudo ./bin/trema send_packets -n 1 -s h5 -d h1
+rvmsudo ./bin/trema send_packets -n 1 -s h6 -d h1
+
+rvmsudo ./bin/trema send_packets -n 1 -s h1 -d h2
+rvmsudo ./bin/trema send_packets -n 1 -s h1 -d h2
