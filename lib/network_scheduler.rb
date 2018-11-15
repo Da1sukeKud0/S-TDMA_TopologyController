5dc7799265cf68de0889d73a319479e2eed6954a
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-11-15 21:22:34 +0900

    [add] NetworkScheduler(Trema:Controllerクラスの継承およびsend_flow_mod_add関数呼び出しテスト段階)

--- lib/network_scheduler.rb
+++ lib/network_scheduler.rb
@@ -1,7 +1,22 @@
 ##
 ## ネットワーク全体でのスケジュール管理を行うクラス
 ##
-class NetworkScheduler
+class NetworkScheduler < Trema::Controller
+
   def initialize
+    @schedule_table = Hash.new
+    for i in Range.new(0, 9)
+      @schedule_table[i] = []
+    end
+  end
+
+  def make_flowmods(flowmod_list)
+    flowmod_list.each do |each|
+      send_flow_mod_add(
+        each[:dpid],
+        match: Match.new(in_port: each[:in_port]),
+        actions: SendOutPort.new(each[:out_port]),
+      )
+    end
   end
 end
