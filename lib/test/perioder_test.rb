69a8cd486d442d0a56e614210ab4806f2133d353
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-11-16 18:33:10 +0900

    [fix] rtc_manager.rbへの移行に向けた仕様変更(テスト済み)

--- lib/test/perioder_test.rb
+++ lib/test/perioder_test.rb
@@ -12,11 +12,48 @@ class Perioder
     if (@period_list.size == 1)
       @lcm = period
       puts "lcm is #{@lcm}"
-    elsif old_lcm = @lcm
-      puts "#{calc_lcm / old_lcm} bai !"
+    else
+      old_lcm = @lcm
+      res = calc_lcm / old_lcm
+      puts "#{res} bai !"
+    end
+    return res
+  end
+
+  ## routeSchedule
+  ## @period_listに新規periodを追加する関数
+  def add_period(period)
+    @period_list.push(period)
+    puts "plist is #{@period_list}"
+    if (@period_list.size == 1)
+      @lcm = period
+      # puts "lcm is #{@lcm}"
+      return 1
+    else
+      old_lcm = @lcm
+      res = calc_lcm / old_lcm
+      # puts "#{res} bai !"
+      return res
     end
   end
 
+  ## @period_listに新規periodを追加した場合の
+  ## @timeslot_tableの倍率を返す関数
+  def add_period?(period)
+    puts "plist is #{@period_list}"
+    if (@period_list.size == 0)
+      @lcm = period
+      puts "lcm is #{@lcm}"
+      return 1
+    else
+      old_lcm = @lcm
+      res = (@lcm.lcm(period)) / old_lcm
+      puts "#{res} bai !"
+    end
+    return res
+  end
+
+  ## @period_listから指定したperiodを1つだけ削除する関数
   def del_period(period)
     for i in Range.new(0, @period_list.size - 1)
       if (@period_list[i] == period)
@@ -28,11 +65,16 @@ class Perioder
     if (@period_list.size == 0)
       @lcm = 0
       puts "timeslot all delete"
-    elsif old_lcm = @lcm
+    else
+      old_lcm = @lcm
       puts "minus #{old_lcm / calc_lcm} bai !"
     end
   end
 
+  private
+
+  ## add_period, del_period
+  ## @period_listの要素全ての最小公倍数を返す関数
   def calc_lcm
     if @period_list.size == 0
       puts "0"
@@ -53,6 +95,8 @@ end
 
 p = Perioder.new
 p.add_period(2)
+puts "add_period?"
+p.add_period?(2)
 puts ""
 p.add_period(3)
 puts ""
