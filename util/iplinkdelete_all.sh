20d401878fac777aa9b6cef71aeced04434bdd43
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-11-27 17:53:59 +0900

    [fix] ip link showにおけるh*とs*_*も削除対象に追加

--- util/iplinkdelete_all.sh
+++ util/iplinkdelete_all.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-sudo ip link show type veth|cut -d" " -f2|cut -d":" -f1|cut -d"@" -f1 |grep 0x | sudo xargs -L 1 -t ip link delete
+sudo ip link show type veth|cut -d" " -f2|cut -d":" -f1|cut -d"@" -f1 |grep -e 0x -e h | sudo xargs -L 1 -t ip link delete
+sudo ip link show type veth|cut -d" " -f2|cut -d":" -f1|cut -d"@" -f1 |grep -E s.*_.* | sudo xargs -L 1 -t ip link delete
 sudo rm /tmp/vhost.h*
