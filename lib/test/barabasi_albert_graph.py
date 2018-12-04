46672a7ccf8e8be14079e3d47471cc7f727b5d74
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-12-04 17:43:22 +0900

    [fix] 画像の逐次表示をオフに・ノード番号を0からではなく1から振る用に修正

--- lib/test/barabasi_albert_graph.py
+++ lib/test/barabasi_albert_graph.py
@@ -10,10 +10,27 @@ import pickle
 
 
 def main():
+    # デフォルト引数処理
+    if sys.argv and len(sys.argv) == 3:
+        switchNum = int(sys.argv[1])
+        complexity = int(sys.argv[2])
+    else:
+        switchNum = 30
+        complexity = 2
     # Barabási-Albertモデルのグラフを生成
-    args = sys.argv
-    G = barabasi_albert_graph(int(args[1]), int(args[2]))
-    # print(G.edges())
+    tmp = barabasi_albert_graph(switchNum, complexity)
+    # ノード番号を1からに
+    edges = []
+    for edge in tmp.edges():
+        e = list(edge)
+        e[0] += 1
+        e[1] += 1
+        edges.append(tuple(e))
+    sorted(edges,key=lambda e:(e[0],e[1]))
+    print edges
+    # edgesから無向グラフ作成
+    G = networkx.Graph()
+    G.add_edges_from(edges)
     # 外部出力
     if os.path.exists('.edges'):
         os.remove('.edges')
@@ -22,7 +39,7 @@ def main():
             f.write(str(l) + "\n")
     networkx.draw(G, with_labels=True)
     pngpath = ".topo_ba.png"
-    networkx.nx_agraph.view_pygraphviz(G, prog='fdp')
+    networkx.nx_agraph.pygraphviz_layout(G, prog='fdp')
     pyplot.tight_layout
     pyplot.savefig(pngpath)
 
