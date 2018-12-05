4722f397417136595b95263dfce006a21bab9105
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-12-05 21:55:17 +0900

    [fix] 12838{@:2}

--- lib/test/rtc_manager_test.rb
+++ lib/test/rtc_manager_test.rb
@@ -12,11 +12,12 @@ class RTCManagerTest
 
   ## main
   ## BAモデルに基づいたトポロジの生成
-  def make_ba_topology(switchNum, complexity, rtcNum)
+  def make_ba_topology(switchNum, complexity)
+    @type = "BA"
     @switchNum = switchNum.to_i
-    @rtcNum = rtcNum.to_i
+    @complexity = complexity.to_i
     ## barabasi_albert_graph.pyを外部実行
-    sh ("python ~/trema/topology/lib/test/barabasi_albert_graph.py #{@switchNum} #{complexity}")
+    sh ("python ~/trema/topology/lib/test/barabasi_albert_graph.py #{@switchNum} #{@complexity}")
     ## 生成されたリンクを@edgesに格納
     File.open(".edges") do |file|
       file.each_line do |l|
@@ -28,7 +29,7 @@ class RTCManagerTest
     end
     ## @edgesからs2sリンクを生成
     @edges.each do |src, dst|
-      puts "src: #{src}, dst: #{dst}"
+      # puts "src: #{src}, dst: #{dst}"
       add_switch2switch_link(src, dst)
     end
     ## switchNum個のホストを同番のスイッチに接続
@@ -38,11 +39,11 @@ class RTCManagerTest
   ## main
   ## 指定回数のスケジューリング探索の実行
   def add_rtcs(num)
+    num = num.to_i
     ## 重複しないようにnum回分のsrc,dstをランダムに選択(periodは重複可)
     srcList = []
     dstList = []
     l = Array.new(@switchNum) { |index| index + 1 }
-    puts l
     popMax = @switchNum
     num.times do
       srcList.push(l.delete_at(rand(popMax)))
@@ -51,15 +52,32 @@ class RTCManagerTest
       popMax -= 1
     end
     ## num回分探索
-    num.times do
+    result = []
+    for n in Range.new(1, num)
       src = srcList.pop
       dst = dstList.pop
       period = rand(4) + 2
+      puts ""
       puts "add_rtc?(src: h#{src}, dst: h#{dst}, period: #{period})"
-      startwatch("add_rtc?呼び出し")
-      @rtcManager.add_rtc?(@hosts["mac" + src.to_s], @hosts["mac" + dst.to_s], period, @topo)
-      stopwatch("スケジューリング終了")
+      # startwatch("add_rtc?呼び出し")
+      st = Time.now
+      tf = @rtcManager.add_rtc?(@hosts["mac" + src.to_s], @hosts["mac" + dst.to_s], period, @topo)
+      puts time = (Time.now - st)
+      # stopwatch("スケジューリング終了")
+
+      ## 計測結果をresultに格納
+      r = Hash.new
+      # r.store("type", @type) ## トポロジタイプ
+      # r.store("snum", @switchNum) ## スイッチ数
+      # r.store("complexity", @complexity) ## 複雑度
+      # r.store("rnum", num) ## RTC数
+      r.store("lnum", @edges.size) ## リンク数
+      r.store("turn", n) ## RTC実行順
+      r.store("time", time) ## 処理時間
+      r.store("tf", tf) ## add_rtc?
+      result.push(r)
     end
+    return result
   end
 
   private
@@ -82,7 +100,6 @@ class RTCManagerTest
   end
 
   ## @topoにs2hのリンクを追加する関数
-  ##
   def add_switch2host_link(hostStats) ##hostStatsはHost型
     l = Hash.new { [].freeze }
     l.store(:type, "switch2host")
@@ -94,7 +111,6 @@ class RTCManagerTest
   end
 
   ## @topoにs2sのリンクを追加する関数
-  ##
   def add_switch2switch_link(dpid_a, dpid_b)
     l = Hash.new { [].freeze }
     l.store(:type, "switch2switch")
@@ -104,14 +120,12 @@ class RTCManagerTest
     @topo.push(l)
   end
 
-  ##
   ## タイマー
   def startwatch(tag)
     @timer = Time.now
     @old_tag = tag
   end
 
-  ##
   ## 前回の呼び出しからの経過時間を測定
   def stopwatch(tag)
     if @timer
@@ -127,14 +141,29 @@ def sh(command)
   @logger.debug(command) if @logger
 end
 
+def output_json(filename, hash)
+  File.open(filename, "a") do |file|
+    JSON.dump(hash, file)
+  end
+end
+
 if __FILE__ == $0
   if (ARGV[0].nil? || ARGV[1].nil?)
-    puts "usage: ruby rtc_manager_test.rb switchNum complexity"
+    puts "usage: ruby rtc_manager_test.rb switchNum complexity rtcNum"
+    puts "use default parameter: switchNum = 30, complexity = 2, rtcNum = 3"
     ARGV[0] = 30
     ARGV[1] = 2
     ARGV[2] = 3
   end
-  rmt = RTCManagerTest.new
-  rmt.make_ba_topology(ARGV[0], ARGV[1], ARGV[2])
-  rmt.add_rtcs(3)
+
+  output = []
+  50.times do
+    rmt = RTCManagerTest.new
+    rmt.make_ba_topology(ARGV[0], ARGV[1])
+    res = rmt.add_rtcs(ARGV[2])
+    puts res
+    output.push(res)
+  end
+  filename = "BA_s#{ARGV[0]}_cplx#{ARGV[1]}_rtc#{ARGV[3]}"
+  output_json(filename, output)
 end
