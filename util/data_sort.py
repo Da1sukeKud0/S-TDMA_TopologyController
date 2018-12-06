12e3b93373f668d7d6212f919d51f0d21f4ac689
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-12-06 21:28:49 +0900

    [fix] 単一の要素でソートし平均時間を算出する処理の追加

--- util/data_sort.py
+++ util/data_sort.py
@@ -7,22 +7,40 @@ class JsonHelper:
     def __init__(self, file_name):
         with open(file_name) as f:
             self.dics = json.load(f)
-            print(self.dics)
+            # print(self.dics)
 
-    def sort_by_turn(self):
-        turn = {}
+    # sortbyの要素毎に処理時間の平均値を算出
+    def sort_by(self, sortby):
+        print("")
+        print("each: " + sortby)
+        result = {}
         for d in self.dics:
-            if (d["turn"] not in turn):
-                turn[d["turn"]] = []
-            turn[d["turn"]].append(d["time"])
-        self.__ave(turn)
+            if (d[sortby] not in result):
+                result[d[sortby]] = []
+            result[d[sortby]].append(d["time"])
+        self.__ave(result)
 
     def __ave(self, dic):
-        for k, v in dic.items():
+        for k, v in sorted(dic.items(), key=lambda x: x[0]):
             ave = sum(v)/len(v)
             print("key: " + str(k) + ", ave: " + str(ave))
 
 
 if __name__ == '__main__':
-    sorter = JsonHelper("BA_s30_cplx2_rtc3.json")
-    sorter.sort_by_turn()
+    jh = JsonHelper("rtcm_test.json")
+    jh.sort_by("turn")
+    jh.sort_by("snum")
+    jh.sort_by("lnum")
+
+"""
+取得したデータは配列内dict
+dictのkey, valの内訳は以下
+r.store("type", @type) ## トポロジタイプ
+r.store("snum", @switchNum) ## スイッチ数
+r.store("complexity", @complexity) ## 複雑度
+r.store("rnum", num) ## RTC数
+r.store("lnum", @edges.size) ## リンク数(switchNum-complexity)*complexityで算出可能
+r.store("turn", n) ## RTC実行順
+r.store("time", time) ## 処理時間
+r.store("tf", tf) ## add_rtc?
+"""
