35cae6e8f47e9a382b5550631be62a48a9a9c7ca
Da1sukeKud0 <yokohamadiceke@yahoo.co.jp>
2018-12-11 15:20:15 +0900

    [fix] Minor fix

--- util/data_sort.py
+++ util/data_sort.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import json
+import numpy
+from matplotlib import pyplot
+from matplotlib.font_manager import FontProperties
+fp = FontProperties(
+    fname="/usr/share/fonts/truetype/fonts-japanese-gothic.ttf")
 
 
 class JsonHelper:
@@ -15,21 +20,35 @@ class JsonHelper:
         print("each: " + sortby)
         result = {}
         for d in self.dics:
+            if not (d["tf"]):
+                continue
             if (d[sortby] not in result):
                 result[d[sortby]] = []
             result[d[sortby]].append(d["time"])
         self.__ave(result)
 
     def __ave(self, dic):
+        arr = []
         for k, v in sorted(dic.items(), key=lambda x: x[0]):
             ave = sum(v)/len(v)
             print("key: " + str(k) + ", ave: " + str(ave))
+            d = [k, ave]
+            arr.append(d)
+        pyplot.plot([i[0] for i in arr], [i[1] for i in arr], "o")
+        pyplot.ylabel(u'during time [s]', fontproperties=fp)
+        pyplot.xlabel(u'xlabel', fontproperties=fp)
+        # pyplot.xticks(
+        # [1.25, 2.25], [u'目盛りは', 'fontproperties=fp'], fontproperties=fp)
+        # pyplot.title(u'タイトルはfontproperties=fp', fontproperties=fp)
+        pyplot.show()
+        # pyplot.savefig("tmp.png")
 
 
 if __name__ == '__main__':
-    jh = JsonHelper("rtcm_test.json")
+    jh = JsonHelper("test/rtcm_test_s5to100_c1to5_rtcr5.json")
     jh.sort_by("turn")
     jh.sort_by("snum")
+    jh.sort_by("cplx")
     jh.sort_by("lnum")
 
 """
@@ -37,10 +56,16 @@ if __name__ == '__main__':
 dictのkey, valの内訳は以下
 r.store("type", @type) ## トポロジタイプ
 r.store("snum", @switchNum) ## スイッチ数
-r.store("complexity", @complexity) ## 複雑度
 r.store("rnum", num) ## RTC数
 r.store("lnum", @edges.size) ## リンク数(switchNum-complexity)*complexityで算出可能
 r.store("turn", n) ## RTC実行順
 r.store("time", time) ## 処理時間
 r.store("tf", tf) ## add_rtc?
+result.push(r)
+if (@type == "BA")
+    r.store("cplx", @complexity) ## 複雑度
+elsif (@type == "tree")
+    r.store("dep", @depth)
+    r.store("fot", @fanout)
+end
 """
